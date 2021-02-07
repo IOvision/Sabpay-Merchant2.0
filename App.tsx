e61c1b2b12dcc5cd4e47483056b0223dcde80b03
Rakshit Singh <rakshit.lko@gmail.com>
2021-02-08 01:15:29 +0530

    setup amplify and added rnc async storage

--- App.tsx
+++ App.tsx
@@ -3,6 +3,14 @@ import { View, Text } from 'react-native'
 import Root from './src/navigation/Root'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
+import Amplify, { Auth } from 'aws-amplify'
+import awsConfig from './aws-exports'
+import AmplifyStorage from './src/models/AmplifyStorage'
+
+Amplify.configure(awsConfig)
+Auth.configure({
+  storage: AmplifyStorage
+})
 
 export default function App() {
   return (
